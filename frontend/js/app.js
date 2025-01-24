6dfa56554da3d2408f318ff6a6a112d109b1cbc7
Xoconoch <93692082+Xoconoch@users.noreply.github.com>
2025-01-24 12:30:25 -0600

    Delete frontend/js directory

--- frontend/js/app.js
+++
@@ -1,9 +0,0 @@
-import { loadAccounts } from './components/load.js';
-import { initSearch } from './components/search.js';
-import { initDownload } from './components/download.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-    loadAccounts();
-    initSearch();
-    initDownload();
-});
